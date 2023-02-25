a5d1406dadb8c0687fe01eef971d9315b50f97fc
Alex Rodgers <90972135+rodgersajb@users.noreply.github.com>
2023-02-25 13:14:56 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+
+function App() {
+
+  // Create three cards: To Do, In Progress, and Done.
+    // Display grid for the cards
+    //for Each card, have tasks displayed that can be moved around based on what card it applies to
+    // 
+
+  
+
+  return (
+    <div className="App">
+      
+    </div>
+  )
+}
+
+export default App
